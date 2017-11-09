955f63cbb54e75ef49d61830c6f05a13edf9f8aa
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2017-11-09 12:50:42 +0800

    Word Search Function Updates

--- server/ELIZAWordSearch.js
+++ server/ELIZAWordSearch.js
@@ -1,3 +1,33 @@
 wordSearch = function(msg, engLexicon) {
-  
+  var wordSearchResult = "";
+  if(msg.indexOf("Which 10") > -1)
+  {
+    var allMatches;
+    if(msg.indexOf("verb") > -1)
+    {
+      allMatches = engLexicon.find({POS: "verb"});
+    }
+    else if(msg.indexOf("noun") > -1)
+    {
+      allMatches = engLexicon.find({POS: "noun"});
+    }
+    else if(msg.indexOf("adjective") > -1)
+    {
+      allMatches = engLexicon.find({POS: "adjective"});
+    }
+    if(allMatches !== undefined)
+    {
+      allMatches = allMatches.fetch();
+      for(index=0 ; index<10 ; index++)
+      {
+        wordSearchResult = wordSearchResult+allMatches[index].Word+", ";
+      }
+      wordSearchResult = "There you go: "+wordSearchResult;
+    }
+    else
+    {
+      wordSearchResult = "Sorry, I got nothing for you.";
+    }
+  }
+  return wordSearchResult;
 };
