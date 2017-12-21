680c423b96d34c0f8a31d515c38dba752a32a71f
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2017-12-21 13:01:19 +0800

    ELIZA Redesign

--- client/main.js
+++ client/main.js
@@ -50,6 +50,10 @@ Template.body.events({
     //event.preventDefault()代表這個事件我們不要執行預設的HTML功能，避免每次按
     //Submit的按鈕都會重新讀取網頁
     event.preventDefault();
+
+    document.getElementById("submittingMsg").style.display = "block";
+
+    setTimeout(hideMsg, 2000);
     //建立一個變數myMsg儲存輸入在網頁中id=myMsg物件的內容(value)
     var myMsg = document.getElementById("myMsg").value;
     //把網頁中id=myMsg的內容設定為一個空的字串
@@ -67,3 +71,7 @@ Template.body.events({
     Meteor.call("resetELIZA");
   }
 });
+
+var hideMsg = function() {
+  document.getElementById("submittingMsg").style.display = "none";
+};
